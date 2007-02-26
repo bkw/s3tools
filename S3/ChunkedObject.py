44f1fdb4ab87495b91a46c8a340e9b1af90d8a75
Michal Ludvig <michal@logix.cz>
2007-02-26 11:53:26 +0000

    S3/ChunkedObject.py: - New class ChunkedObjectReader - Some changes to ChunkedObjectWriter.get_info() format
    git-svn-id: https://s3tools.svn.sourceforge.net/svnroot/s3tools/s3cmd/branches/s3cmd-chunked@92 830e0280-6d2a-0410-9c65-932aecc39d9d

--- S3/ChunkedObject.py
+++ S3/ChunkedObject.py
@@ -9,6 +9,9 @@ from Crypto.Hash import SHA
 from S3 import *
 from Streamer import *
 
+class ChunkedObjectError(Exception):
+	pass
+
 class ChunkedObjectFile(object):
 	def __init__(self, prefix, max_size, autoremove = True, big_hash = None):
 		self._max_size = max_size
@@ -20,12 +23,13 @@ class ChunkedObjectFile(object):
 		self._autoremove = autoremove
 		self._sha1 = SHA.new()
 		self._big_hash = big_hash
+		self._final_size = None
 
 	def free_space(self):
 		return self._max_size - self._f.tell()
 
 	def size(self):
-		return self._f.closed and os.stat(self._f_name).st_size or self._f.tell()
+		return self._f.closed and self._final_size or self._f.tell()
 
 	def append(self, data):
 		if len(data) <= self.free_space():
@@ -45,6 +49,8 @@ class ChunkedObjectFile(object):
 	def close(self):
 		if not self._f.closed:
 			self._f.close()
+		if self._final_size is None:
+			self._final_size = os.stat(self._f_name).st_size
 
 	def name(self):
 		return self._f_name
@@ -120,9 +126,9 @@ class ChunkedObjectWriter(object):
 	def get_info(self, stream = None):
 		if not stream:
 			stream = StringIO()
-		stream.write("big_sha1=%s\n" % self.get_big_sha1())
+		stream.write("big_hash: sha1=%s\n" % self.get_big_sha1())
 		for chunk in self._chunklist:
-			stream.write("id=%s	size=%s	sha1=%s\n" % (
+			stream.write("chunk: id=%s size=%s sha1=%s\n" % (
 				chunk["obj_id"], chunk["size"], chunk["sha1"]))
 		stream.seek(0)
 		return stream
@@ -134,39 +140,72 @@ class ChunkedObjectWriter(object):
 		self._chunk_size = new_size
 		self._current_chunk.set_max_size(new_size)
 
+class ChunkedObjectReader(object):
+	_read_chunk_size = 4096
+
+	def __init__(self, chunk_request_callback = None, callback_arg = None):
+		self._callback = chunk_request_callback
+		self._callback_arg = callback_arg
+		self._big_sha1 = SHA.new()
+		self._info_big_hash = {}
+		self._info_chunks = []
+	
+	def get_stream(self, info_stream, output_stream):
+		r_big_hash = re.compile("^big_hash: (\w+)=([0-9a-f]+)$")
+		r_chunk_1 = re.compile("^chunk: ")
+		r_chunk_2 = re.compile("(\w+)=([^\s]*)")
+		r_comment = re.compile("^#")
+		r_empty = re.compile("^\s*$")
+		for line in info_stream:
+			if r_comment.match(line) or r_empty.match(line):
+				continue
+
+			if r_chunk_1.match(line):
+				self._info_chunks.append({})
+				for match in r_chunk_2.finditer(line):
+					self._info_chunks[-1][match.group(1)] = match.group(2)
+
+			m = r_big_hash.match(line)
+			if m:
+				if m.group(1) != "sha1":
+					raise ChunkedObjectError("Unknown 'big_hash' algorithm '%s'. Expected sha1." % m.group(1))
+				self._info_big_hash[m.group(1)] = m.group(2)
+				continue
+		print "Chunks: %s" % self._info_chunks 
+		print "BigHash: %s" % self._info_big_hash
+	
 if __name__ == "__main__":
 	def _put_callback(arg, chunk):
-		print "CallBack: %s (%d bytes)" % (
+		if not arg.has_key("workdir"):
+			arg["workdir"] = mktmpdir("/tmp/s3cmd/CB-")
+			arg["seq"] = 0
+		newname = "%s/%05d" % (arg["workdir"], arg["seq"])
+		arg["seq"] += 1
+		os.rename(chunk.name(), newname)
+		print "Renamed %s to %s (%d bytes)" % (
 			chunk.name(),
+			newname,
 			chunk.size())
-		return os.path.basename(chunk.name())
+		return newname.replace("/tmp/s3cmd/", "", 1)
 	
-	w = ChunkedObjectWriter(chunk_done_callback = _put_callback, callback_arg = {})
+	def _get_callback(arg, id):
+		print "Callback: request for '%s'" % (id)
+		return Streamer(open("/tmp/s3cmd/" + id))
+
+	if not os.path.exists("/tmp/s3cmd"):
+		os.mkdir("/tmp/s3cmd")
+
+	w = ChunkedObjectWriter(tmp_dir = "/tmp/s3cmd", chunk_done_callback = _put_callback, callback_arg = {})
 	w.set_chunk_size(500)
 	w.put_stream(open("/etc/passwd"))
 	w.commit()
 	print "# ChunkedObject info:\n" + w.get_info().read()
 
-#	f = ChunkedObject("s3://s3py-1/chunked.file", "w")
-#	f.chunk_size = 128
-#	f.put("/etc/passwd")
-#	f.close()
-#
-#	f = ChunkedObject("s3://s3py-1/stdin", "w")
-#	f.chunk_size = 256
-#	while True:
-#		chunk = sys.stdin.read(160)
-#		f.append(chunk)
-#		if len(chunk) < 160:
-#			break
-#	f.close()
-#
-#	f = ChunkedObject("s3://s3py-1/stdin-streamed", "w")
-#	f.chunk_size = 256
-#	f.tmp_dir = "/tmp"
-#	f.put_stream(sys.stdin)
-#	f.close()
-#
+	r = ChunkedObjectReader(chunk_request_callback = _get_callback)
+	outfile = open(mktmpfile("/tmp/s3cmd/outfile-"), "w")
+	r.get_stream(w.get_info(), outfile)
+	print "Saved file %s (%d bytes)" % (outfile.name, os.stat(outfile.name).st_size)
+
 #	f = ChunkedObject("s3://s3py-1/stdin", "r")
 #	out = open("/tmp/stdin.out", "w")
 #	size_total = 0L
